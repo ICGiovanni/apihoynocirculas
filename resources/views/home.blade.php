03a235d22e6707c1b8aaa5d48d3cc84cce9753fb
Giovanni Estrada <ic.giovanni.estrada@gmail.com>
2023-08-25 11:10:28 -0600

    Instalacion docker y authenticacion en la plataforma

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged JAVI!
                 </div>
             </div>
         </div>
